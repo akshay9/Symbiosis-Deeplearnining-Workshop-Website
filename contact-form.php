ca96c657b76718a81ded6eb498d830f39219762b
Avinash Barfa <avibarfa98@gmail.com>
2018-11-05 12:43:01 +0530

    Payment Details Updated.

--- contact-form.php
+++ contact-form.php
@@ -1,26 +1,31 @@
 <?php
-if(isset($_POST['submit']))
+/*if(isset($_POST['submit']))*/
 {
 		$name=$_POST['name'];
 		$mobile=$_POST['mobile'];
 		$email=$_POST['email'];		
 		$message=$_POST['message'];
+		$subject = $_POST['subject'];
 		$email=filter_var($email,FILTER_SANITIZE_EMAIL);
 		$email=filter_var($email,FILTER_VALIDATE_EMAIL);
-		$mail_to="avinash.barfa@outlook.com";
-		$headers = "CC: avibarfa98@gmail.com";
+		$mail_to="symbiosisdlworkshop@symbiosisdlworkshop.com";
+		$headers .= 'From: symbiosisdlworkshop@symbiosisdlworkshop.com'.PHP_EOL;
+		$headers .= 'Cc: pooja.kamat@sitpune.edu.in'.PHP_EOL;
+		$headers .= 'Bcc: avibarfa@gmail.com'.PHP_EOL;
 		{
-			$from="From : $name\n";
-			$subject="Message sent using your Contact Form from your website 'licmohanbarfa.in':\n\n";
+			$from="Contact Form - symbiosisdlworkshop.com\n";
+			$name="Name : $name\n";
+			$subject="Subject : $subject\n";
 			$mobile="Mobile : $mobile\n";
 			$email="Email : $email\n";
 			$message=wordwrap($message,5000);
-			$reply="-------------------message over-------------------------\n\nPlease Get in touch With $name..\n\n\nSource:symbiosisdlworkshop.com\nHosted by-Avinash Barfa\nDelivered to- Mohan Barfa";
-			$detail="$subject\n\n$from\n$email\n$mobile\n\nMessage :\n$message\n\n\n\n\n$reply";
+			$reply="-------------------message over-------------------------\n\nSource: symbiosisdlworkshop.com";
+
+			$detail="\n$name\n$subject\n$email\n$mobile\nMessage :\n$message\n\n\n$reply";
 			mail($mail_to,$from,$detail,$headers);
-			echo "<br><br><center><h1>Soon, we will resolve your query.</h1></center>";
+			echo "Messent Sent Successfully !!";
 		}
 } else {
-	echo "Some Connectivity Issue";
+	echo "Connectivity Issue Occured";
 }
 ?>
